55a885b09a1e8ba5e71062c8df9525c36f977d2e
daniel-amora <daniel_amora@edu.unifor.br>
2020-12-03 18:32:27 -0300

    continentes

--- src/app/continente.ts
+++ src/app/continente.ts
@@ -1,1 +1,12 @@
- 
+export class Continente{
+
+  continente:string;
+  foto:string;
+  confirmados:string;
+  mortes:string;
+  recuperados:string
+  ativos:string;
+  gastos:string;
+  lockdown:string;
+  leitosocupados:string;
+  }
